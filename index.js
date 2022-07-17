25e1f9f19a6509776a17e3c5fd27dedd3b48e284
--replace-all <--replace-all>
2022-07-17 11:55:32 +0500

    yubotidi

---
+++ index.js
@@ -0,0 +1,20 @@
+// let str = "AABBDCCCNMMMJLLL"
+// let num = 1
+// let string = ''
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] == str[i+1]) {
+//     num++
+//   }else{
+//     string += `${str[i]}${num == 1 ? '' : num}`
+//     num = 1
+//   }   
+// }
+// console.log(string);
+
+// let str_num = "sd65ew9rt32as10re9thhil64ghfew8"
+// let resNum = str_num.match(/[0-9]/gi).join('')
+// let resStr = str_num.match(/[a-z]/gi).join('')
+// console.log(+resNum, resStr);
+
+
+
